40ca59e3a9491daf636313d7bfbaa06e7d8e41b7
Dimitri Bervoets <dimitri.bervoets@simac-access.com>
2025-11-25 18:58:40 +0100

    Add support for conjured items.

--- php/src/ItemUpdater/ConjuredUpdater.php
+++ php/src/ItemUpdater/ConjuredUpdater.php
@@ -4,6 +4,18 @@ namespace GildedRose\ItemUpdater;
 
 use GildedRose\Item;
 
-class ConjuredUpdater  extends DefaultItemUpdater
+class ConjuredUpdater extends DefaultItemUpdater
 {
+    public function update(Item $item): void
+    {
+        if ($item->quality > 1) {
+            $item->quality = $item->quality - 2;
+        }
+
+        $item->sellIn--;
+
+        if ($item->sellIn < 0 && $item->quality > 1) {
+            $item->quality = $item->quality - 2;
+        }
+    }
 }
